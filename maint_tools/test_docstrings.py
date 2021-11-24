e216040bf4fbf98684c55e6cc5373dab16b4d868
reshamas <reshama.stat@gmail.com>
2021-11-24 13:24:41 -0500

    comment out paired_distances function

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -142,7 +142,7 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.pairwise.manhattan_distances",
     "sklearn.metrics.pairwise.nan_euclidean_distances",
     "sklearn.metrics.pairwise.paired_cosine_distances",
-    "sklearn.metrics.pairwise.paired_distances",
+    # "sklearn.metrics.pairwise.paired_distances",
     "sklearn.metrics.pairwise.paired_euclidean_distances",
     "sklearn.metrics.pairwise.paired_manhattan_distances",
     "sklearn.metrics.pairwise.pairwise_distances_argmin",
